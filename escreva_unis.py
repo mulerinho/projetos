a892fa505a5c53911363c11eb7a133020d7b82ef
mulerinho <alan_mulero@outlook.com>
2022-03-09 08:06:47 -0300

    Exercicio 1 ciclo 3 Unis

---
+++ escreva_unis.py
@@ -0,0 +1,9 @@
+# Trabalho faculdade, escreva: UNIS-MG verticalmebte.
+#Declarando variaveis:
+apresenta_resultado = ''
+frase = 'UNIS-MG'
+# Criando algotitimo.
+for c in frase:
+    apresenta_resultado = c
+    for d in c:
+        print(apresenta_resultado)
